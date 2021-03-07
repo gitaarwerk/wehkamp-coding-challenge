1d31a2506c782125bdc078d8157a7069af613f64
Gitaarwerk <>
2021-03-07 14:03:37 +0100

    Initial commit

---
+++ src/components/ProductPage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../Header";
+import { Product } from "../Product";
+import { Footer } from "../Footer";
+
+export const ProductPage = () => (
+  <main>
+    <Header />
+    <Product />
+    <Footer />
+  </main>
+);
